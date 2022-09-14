a57fa2aca807dd4f748854ac2d55003a7a6b8d58
EduardoRdz007 <edurdz010@gmail.com>
2022-09-13 21:03:09 -0500

    feat: creation  of the first project with Expo

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Eduardo Rodriguez 191045</Text>
+      <Text>Universidad Tecnologica de Aguascalientes</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0099CC',
     alignItems: 'center',
     justifyContent: 'center',
   },
